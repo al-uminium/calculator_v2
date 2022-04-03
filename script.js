74b08dab9dc7ae2fb9a51511e0d0559bf28d71e3
al-uminium <79945949+al-uminium@users.noreply.github.com>
2022-04-03 21:00:39 +0800

    skeleton css initiated

---
+++ script.js
@@ -0,0 +1,22 @@
+//
+
+//basic math functions
+
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+const exponential = (a,b) => a**b;
+
+
+const test = () => {
+    console.log(
+        add(2,3),
+        subtract(5,4),
+        multiply(2,5),
+        divide(10,2.3),
+        exponential(2,4)
+    )
+}
+
+
